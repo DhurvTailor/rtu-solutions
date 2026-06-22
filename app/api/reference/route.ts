ce7cff9509dbe6b4e65600c3c59932a4075f3eac
DhurvTailor <tailordhurv4005@gmail.com>
2026-06-22 17:39:16 +0530

    yes

--- app/api/reference/route.ts
+++ app/api/reference/route.ts
@@ -6,6 +6,5 @@ export async function GET() {
   return NextResponse.redirect(
     new URL("/openapi.json", process.env.NEXTAUTH_URL || "http://localhost:3000")
     );                      
-}       
-
+      }
 
