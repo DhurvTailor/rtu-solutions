10035c42ada6b8c1f6b866ffc9de3dded40e8b13
DhurvTailor <tailordhurv4005@gmail.com>
2026-07-08 15:12:58 +0530

    fix robots.txt to allow Googlebot access to thumbnail and feed

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,37 @@
+// import { MetadataRoute } from "next";
+
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: {
+//       userAgent: "*",
+//       allow: "/",
+//       disallow: ["/admin", "/api/", "/checkout", "/profile"],
+//     },
+//     sitemap: "https://www.rtu-solutions.me/sitemap.xml",
+//   };
+// }
+
+
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/admin", "/api/", "/checkout", "/profile"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: ["/", "/api/thumbnail", "/api/product-feed"],
+        disallow: ["/admin", "/api/", "/checkout", "/profile"],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: ["/", "/api/thumbnail", "/api/product-feed"],
+        disallow: ["/admin", "/checkout", "/profile"],
+      },
+      {
+        userAgent: "Googlebot-Image",
+        allow: ["/", "/api/thumbnail"],
+      },
+    ],
     sitemap: "https://www.rtu-solutions.me/sitemap.xml",
   };
 }
