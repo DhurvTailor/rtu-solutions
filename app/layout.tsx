dc41dbdf73a495864d4b49381781c8e8418396e8
DhurvTailor <tailordhurv4005@gmail.com>
2026-08-08 12:12:43 +0530

    yes

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,10 +61,10 @@ export default function RootLayout({
     >
       <head>
         <script src="https://checkout.razorpay.com/v1/checkout.js" async></script> 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4312176557759808"
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4312176557759808"
      crossOrigin="anonymous"></script>
-     <meta name="google-adsense-account" content="ca-pub-4312176557759808"></meta>
-      </head>
+      <meta name="google-adsense-account" content="ca-pub-4312176557759808"></meta> 
+     </head>
       <body className="bg-white text-black flex flex-col min-h-screen">
         <AuthProvider>{children}</AuthProvider>
       </body>
